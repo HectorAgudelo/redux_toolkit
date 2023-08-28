1667827517380604b0b51c6a9ccf4c4af19bad28
Hector Agudelo <haacny86@gmail.com>
2023-08-28 17:30:18 -0400

    simple counter with add value input and reset button

--- reduc_toolkit/src/features/counter/Counter.js
+++ reduc_toolkit/src/features/counter/Counter.js
@@ -1,23 +1,38 @@
-import React from 'react'
-import { useSelector,useDispatch } from 'react-redux'
-import {increment, decrement} from "./counterSlice"
-
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { increment, decrement, reset, incrementByAmount } from './counterSlice';
+import { useState } from 'react';
 
 const Counter = () => {
-    const count = useSelector((state)=> state.counter.count)
-    console.log(count)
-    const dispatch = useDispatch()
+  const count = useSelector((state) => state.counter.count);
+  console.log(count);
+  const dispatch = useDispatch();
+  const [incrementAmount, setIncrementAmount] = useState(0);
+  const addValue = Number(incrementAmount) || 0;
+
+  const resetAll = () => {
+    setIncrementAmount(0);
+    dispatch(reset());
+  };
+
   return (
     <section>
-        <p>
-            {count}
-        </p>
-        <div>
-            <button onClick={()=>dispatch(increment())}>Increment</button>
-            <button onClick={()=>dispatch(decrement())}>Decrement</button>
-        </div>
+      <p>{count}</p>
+      <div>
+        <button onClick={() => dispatch(increment())}>Increment</button>
+        <button onClick={() => dispatch(decrement())}>Decrement</button>
+      </div>
+      <input
+        type='text'
+        value={incrementAmount}
+        onChange={(e) => setIncrementAmount(e.target.value)}
+      />
+      <button onClick={() => dispatch(incrementByAmount(addValue))}>
+        Add Value
+      </button>
+      <button onClick={resetAll}>Reset</button>
     </section>
-  )
-}
+  );
+};
 
-export default Counter
+export default Counter;
